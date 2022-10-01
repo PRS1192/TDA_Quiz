08196442a7b80ac45fcea89b1e65c59838210c46
PRS1192 <113729247+PRS1192@users.noreply.github.com>
2022-10-01 20:11:59 +0100

    Updated v3

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //Add database of questions
 
-let quizData = [
+let questionDatabase = [
   {
     question: "1) The series is set in which city?",
     a: "Los Angeles",
     b: "New York City",
     c: "Miami",
     d: "Seattle",
-    correct: "b",
+    correct: "buttonB",
   },
   {
     question: "2) What pet did Ross own?",
@@ -15,7 +15,7 @@ let quizData = [
     b: "A rabbit called Lancelot",
     c: "A monkey named Marcel",
     d: "A lizard named Alistair",
-    correct: "c",
+    correct: "buttonC",
   },
   {
     question: "3) What is Monica skilled at?",
@@ -23,7 +23,7 @@ let quizData = [
     b: "Cooking",
     c: "American Football",
     d: "Singing",
-    correct: "b",
+    correct: "buttonB",
   },
   {
     question: "4) What song is Phoebe best known for? ",
@@ -31,7 +31,7 @@ let quizData = [
     b: "Stinky Dog",
     c: "Smelly Cat",
     d: "All of the above",
-    correct: "c",
+    correct: "buttonC",
   },
   {
     question: "5) What does Joey never share?",
@@ -39,7 +39,7 @@ let quizData = [
     b: "His information",
     c: "His food",
     d: "His DVDs",
-    correct: "c",
+    correct: "buttonC",
   },
   {
     question: "6) What is Chandler's middle name?",
@@ -47,7 +47,7 @@ let quizData = [
     b: "Jason",
     c: "Kim",
     d: "Zachary",
-    correct: "a",
+    correct: "buttonA",
   },
   {
     question: "7) Who did Rachel almost marry?",
@@ -55,7 +55,7 @@ let quizData = [
     b: "Joshua",
     c: "Barry",
     d: "Paul",
-    correct: "c",
+    correct: "buttonC",
   },
   {
     question: "8) How many seasons of Friends are there?",
@@ -63,7 +63,7 @@ let quizData = [
     b: "28",
     c: "10",
     d: "2",
-    correct: "c",
+    correct: "buttonC",
   },
   {
     question: "9) What is janice most likely to say as she enters a room?",
@@ -71,7 +71,7 @@ let quizData = [
     b: "Get me a coffee, pronto!",
     c: "No way José",
     d: "Oh... my... God!!",
-    correct: "d",
+    correct: "buttonD",
   },
   {
     question: "10) Who was Chandler's TV magazine always addressed to?",
@@ -79,7 +79,7 @@ let quizData = [
     b: "Chanandler Bang",
     c: "Chanandler Bong",
     d: "Chanandler Beng",
-    correct: "c",
+    correct: "buttonC",
   },
 ];
 
@@ -96,75 +96,86 @@ function showItems(element) {
 
 let posterFriends = document.getElementById("posterFriends");
 let initialInfo = document.getElementById("initialInfo");
-let quiz = document.getElementById("quiz");
-let answerEls = document.querySelectorAll(".answer");
-let questionEl = document.getElementById("question");
-let a_text = document.getElementById("a_text");
-let b_text = document.getElementById("b_text");
-let c_text = document.getElementById("c_text");
-let d_text = document.getElementById("d_text");
-let submitBtn = document.getElementById("submit");
-let startBtn = document.getElementById("start");
-let currentQuiz = 0;
-let score = 0;
+let submitButton = document.getElementById("submit");
+let startButton = document.getElementById("start");
+let quizBox = document.getElementById("quizContainerBox");
+let questionSelection = document.getElementById("questionText");
+
+let answerSelectionA = document.getElementById("answerA");
+let answerSelectionB = document.getElementById("answerB");
+let answerSelectionC = document.getElementById("answerC");
+let answerSelectionD = document.getElementById("answerD");
+let radioBtnClass01All = document.querySelectorAll(".radioBtnClass01"); // querySelectorAll selects all specified elements from HTML code (as seen between double quotation marks = HTML Class [hence the preceding full-stop])
+
+let score = 0; //Starts score on zero
+let currentQuestionNo = 0; //Starts current question number on zero (until start button clicked)
 
 // initial page start-up - hide quiz and submit
 
-hideItems(quiz);
+hideItems(quizBox);
 
-// When Start button is clicked
-startBtn.addEventListener("click", function () {
-  showItems(quiz);
-  hideItems(startBtn);
+// When Start button is clicked - show and hide the following:
+startButton.addEventListener("click", function () {
+  showItems(quizBox);
+  hideItems(startButton);
   hideItems(posterFriends);
   hideItems(initialInfo);
 });
 
-// Load quiz with initially no radio buttons checked
+// Define a function to load a question from the question database depending on what no. question currently on
 
-loadQuiz();
-function loadQuiz() {
-  deselectAnswers();
-  let currentQuizData = quizData[currentQuiz];
-  questionEl.innerText = currentQuizData.question;
-  a_text.innerText = currentQuizData.a;
-  b_text.innerText = currentQuizData.b;
-  c_text.innerText = currentQuizData.c;
-  d_text.innerText = currentQuizData.d;
+function loadQuestion() {
+  // Run: 'Deselect checked radio buttons' (function defined below) everytime the load question function has been run
+  deselectRadioButtons();
+
+  //  Define Function: 'Deselect checked radio buttons'
+  function deselectRadioButtons() {
+    radioBtnClass01All.forEach(( radioCheckbox) => ( radioCheckbox.checked = false)); // output = to uncheck the radiobuttons (i.e. false) --  input = output for all the radio buttons
+  }
+
+  // Extract/use question+answers text from questionDatabase depending on current question no.
+  let currentQuestionData = questionDatabase[currentQuestionNo];
+  questionSelection.innerText = currentQuestionData.question; // .innerText allows us to extract and use text from within the Javascript file
+  answerSelectionA.innerText = currentQuestionData.a;
+  answerSelectionB.innerText = currentQuestionData.b;
+  answerSelectionC.innerText = currentQuestionData.c;
+  answerSelectionD.innerText = currentQuestionData.d;
 }
-function deselectAnswers() {
-  answerEls.forEach((answerEl) => (answerEl.checked = false));
-}
+
+// Function checks and returns answer selected by the user
 function getSelected() {
-  let answer;
-  answerEls.forEach((answerEl) => {
-    if (answerEl.checked) {
-      answer = answerEl.id;
+  radioBtnClass01All.forEach(( radioCheckbox) => {
+    if ( radioCheckbox.checked) {
+      answerSelected =  radioCheckbox.id;
     }
   });
-  return answer;
+  return answerSelected; 
 }
 
-// Check/Count score when correct answer and submit button is clicked
+// Run function to load inital/first question
+loadQuestion();
 
-submitBtn.addEventListener("click", () => {
-  let answer = getSelected();
-  if (answer) {
-    if (answer === quizData[currentQuiz].correct) {
-      score++; //Adds additional 1 point to score
+// When submit button is clicked check/count score when correct answer has been chosen
+submitButton.addEventListener("click", () => {
+  let answerSelected = getSelected();
+  if (answerSelected) {
+    if (answerSelected === questionDatabase[currentQuestionNo].correct) { // Compares selected answer by user to correct answer stored in database
+      score++; // Adds additional 1 point to score when answer is correct
     }
 
-    // Run next question if no. of Q's answered is less than database
-    currentQuiz++;
-    if (currentQuiz < quizData.length) {
-      loadQuiz();
+    // Add 1 to current question no. after submit button clicked
+    currentQuestionNo++;
+
+    // Load the next question if current question no. is less than no. of Q's in database
+    if (currentQuestionNo < questionDatabase.length) {
+      loadQuestion();
     }
 
-    // Else condition - run if no more Q's left.
+    // Else condition - show score if no more Q's left and provide 'Retry' Button
     else {
-      quiz.innerHTML = `
-           <h2>Score ${score}/${quizData.length} </h2>
+      quizBox.innerHTML = `
            <button onclick="location.reload()">Retry</button>
+           <h2>Score ${score}/${questionDatabase.length} </h2>
            `;
     }
   }
